eba7effde10fcce3ab341864731960f1da5afecb
shahedCSSe313 <79035775+shahedCSSe313@users.noreply.github.com>
2021-03-20 18:22:39 +0000

    Add files via upload

---
+++ shahed/submition.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    
+    
+    <from>
+    <fieldset>
+    <Table>
+      <tr>
+        <td >Name:</td>
+        <td> <input/></td>
+      </tr>
+      <tr>
+        <td>UserName:</td>
+        <td> <input/></td>
+      </tr>
+      <tr>
+        <td>password:</td>
+        <td> <input/></td>
+      </tr>
+      <tr>
+        <td>Confram password:</td>
+        <td> <input/></td>
+      </tr>
+      <tr>
+        <td>Email:</td>
+        <td> <input/></td>
+      </tr>
+      <tr>
+        <td>Phone:</td>
+        <td> <input style="width:50px"/></td><td>-<input style ="width:100px"/></td>
+      </tr>
+      <tr>
+        <td>Address:</td>
+        <td> <input/></td>
+      </tr>
+      <!-- <tr >
+      <td calspan="2"><input/>-<input/></td>      
+      </tr> -->
+      <tr>
+        <td>Birth Date:</td>
+        <td> <input/></td>
+      </tr>
+      <tr>
+        <td>Gender:</td>
+        <td><input type ="radio" name="gender"/>male</td><td><input type ="radio" name="gender"/>Female</td>
+        <!-- <td><input type ="radio" name="gender"/>male</td><td><input type ="radio" name="gender"/>Female</td> -->
+      </tr>
+      <tr>
+      <td>Bio:</td>
+      <td><textarea name="" id="" cols="30" rows="10"></textarea></td>
+      </tr>
+      <tr>
+      <td><input text-align="center" type="submit" value="submit"/></td>
+      </tr>
+
+    </Table>
+  </fieldset>
+    </from>
+
+</body>
+</html>
